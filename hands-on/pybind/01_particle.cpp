145501ad482df0a561417cce563ea8363f82dcbc
Aurora Perego <aurora.perego@cern.ch>
2024-10-25 13:56:53 +0200

    pybind

--- hands-on/pybind/01_particle.cpp
+++ hands-on/pybind/01_particle.cpp
@@ -5,17 +5,45 @@ namespace py = pybind11;
 
 // Step 1: Define a class representing a particle
 class Particle {
+  float energy_;
+  float mass_;
 public:
-  // Step 1.1: Define getters and setters for the data members
+  Particle() = default;
+  Particle(float e, float m) : energy_(e), mass_(m) {}
 
-  // Step 2: Write methods returning the energy and invariant mass of the particle
+  // Step 1.1: Define getters and setters for the data members
+  void setEnergy(float e) { energy_ = e; }
+  float getEnergy() const { return energy_; }
+
+  void setMass(float m) { mass_ = m; }
+  float getMass() const { return mass_; }
+
+  // Step 2: Method to return the energy of the particle
+  float calculateEnergy(float momentum, float c = 299792458.0f) {
+      // Using E^2 = (pc)^2 + (mc^2)^2 formula
+      return sqrt((momentum * c) * (momentum * c) + (mass_ * c * c) * (mass_ * c * c));
+  }
+
+  // Step 2: Method to return the invariant mass of the particle
+  float calculateInvariantMass(float momentum, float c = 299792458.0f) {
+      // Using m^2c^4 = E^2 - (pc)^2, rearranging to m = sqrt(E^2/c^4 - p^2/c^2)
+      float E = calculateEnergy(momentum, c);
+      return sqrt((E * E) / (c * c * c * c) - (momentum * momentum) / (c * c));
+  }
 };
 
 // Step 3: Write a function returning the distance between two particles
-double distance(/* ... */) {}
+double distance(Particle a, Particle b) { return a.getEnergy() - b.getEnergy();};
 
 PYBIND11_MODULE(HEP, m) {
   // Step 4: Bind the Particle class
+  py::class_<Particle>(m, "Particle")
+    .def(py::init<>())
+    .def(py::init<float, float>())
+    .def("getEnergy", &Particle::getEnergy)
+    .def("setEnergy", &Particle::setEnergy);
 
   // Step 5: Bind the distance function
+  m.def("distance", &distance);
+
 }
--- hands-on/pybind/04_list_accumulate.cpp
+++ hands-on/pybind/04_list_accumulate.cpp
@@ -1,4 +1,5 @@
-
+#include <iostream>
+#include <numeric>
 #include <pybind11/embed.h>
 
 namespace py = pybind11;
@@ -8,9 +9,17 @@ namespace py = pybind11;
 
 int main () {
   // Step 1: Create an interpreter
-  
+  py::scoped_interpreter guard{};
+  py::list li;
+
+  for (int i = 0; i <= 10; ++i)
+    li.append(i);
+
   // Step 3: Create a Python list and fill it with integers from 0 to 10
-  // Hint: Use the ranges library and C++20 views 
+  // Hint: Use the ranges library and C++20 views
 
   // Step 4: Compute the sum of the list's elements and print the result
+  auto sum = std::accumulate(li.begin(), li.end(), 0);
+  std::cout << sum << '\n';
+
 }
